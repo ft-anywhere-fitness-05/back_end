b65284d29189767e3f986d86909ddb20830e7048
MichaelHabermas <michaelghabermas@gmail.com>
2021-06-19 08:08:11 -0400

    heroku push

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
+// const express = require('express');
+// const cors = require('cors');
+// const path = require('path');
 const server = require('./api/server');
 
-server.use(cors());
-server.use(express.static(path.join(__dirname, 'client/build'))); // static assets
+// server.use(cors());
+// server.use(express.static(path.join(__dirname, 'client/build'))); // static assets
 
 if (process.env.NODE_ENV === 'production') {
 	console.log('this means this code is deployed');
